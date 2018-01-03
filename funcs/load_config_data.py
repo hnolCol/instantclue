3bc9d4b91c49040ccd108d35c88bf6e8618861c8
hnolCol <h.nolte@uni-koeln.de>
2018-01-03 22:42:44 +0100

    Delete load_config_data.py

--- funcs/load_config_data.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import os 
-def load_config_file(path='D:\MS_PYStasi\settings\default_settings.ini'):
-
-     config = configparser.ConfigParser()
-     config.read(path)
-
-     return config._sections 
